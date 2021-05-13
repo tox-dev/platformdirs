66d836cb84903ea8fbf436f7cdd112728ad0e987
Julian Berman <Julian@GrayVines.com>
2021-05-13 07:42:24 -0400

    2.0.0a1

--- platformdirs.py
+++ platformdirs.py
@@ -12,7 +12,7 @@ See <https://github.com/platformdirs/platformdirs> for details and usage.
 # - Mac OS X: http://developer.apple.com/documentation/MacOSX/Conceptual/BPFileSystem/index.html
 # - XDG spec for Un*x: https://standards.freedesktop.org/basedir-spec/basedir-spec-latest.html
 
-__version__ = "2.0.0a5"
+__version__ = "2.0.0a1"
 __version_info__ = 2, 0, 0
 
 
