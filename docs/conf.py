7a2c511e6a4832aff2fd42aa586e345fba05e405
Bernát Gábor <gaborjbernat@gmail.com>
2021-07-16 23:25:35 +0100

    Try furo
    Signed-off-by: Bernát Gábor <gaborjbernat@gmail.com>

--- docs/conf.py
+++ docs/conf.py
@@ -1,5 +1,3 @@
-import sphinx_rtd_theme
-
 from platformdirs.version import __version__
 
 author = "The platformdirs team"
@@ -12,11 +10,4 @@ extensions = [
     "sphinx.ext.autosectionlabel",
     "sphinx.ext.viewcode",
 ]
-html_theme = "sphinx_rtd_theme"
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
-html_theme_options = {
-    "canonical_url": "https://platformdirs.readthedocs.io/",
-    "display_version": True,
-    "prev_next_buttons_location": "bottom",
-    "sticky_navigation": True,
-}
+html_theme = "furo"
