401761516c719d94bc4b2b1096735a329a7b83bc
Pablo Prietz <pablo@prietz.org>
2021-07-23 14:33:11 +0200

    Tests: Make sure that there is a "path" variant for every "dir" variant

--- tests/conftest.py
+++ tests/conftest.py
@@ -13,25 +13,17 @@ PROPS = (
     "site_config_dir",
 )
 
-PROPS_PATH = (
-    "user_data_path",
-    "user_config_path",
-    "user_cache_path",
-    "user_state_path",
-    "user_log_path",
-    "site_data_path",
-    "site_config_path",
-)
-
 
 @pytest.fixture(params=PROPS)
 def func(request: SubRequest) -> str:
     return cast(str, request.param)
 
 
-@pytest.fixture(params=PROPS_PATH)
+@pytest.fixture(params=PROPS)
 def func_path(request: SubRequest) -> str:
-    return cast(str, request.param)
+    prop = cast(str, request.param)
+    prop = prop.replace("_dir", "_path")
+    return prop
 
 
 @pytest.fixture()
