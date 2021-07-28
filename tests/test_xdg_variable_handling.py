2df00b9f057e389bdfe7f6884067a351fb4bd532
Pablo Prietz <pablo@prietz.org>
2021-07-28 23:49:16 +0200

    Test XDG variable handling on Linux

---
+++ tests/test_xdg_variable_handling.py
@@ -0,0 +1,51 @@
+import os
+import platform
+import typing
+
+import pytest
+
+import platformdirs
+
+
+class XDGVariable(typing.NamedTuple):
+    name: str
+    default_value: str
+
+
+DIR_CUSTOM = "/tmp/custom-dir"
+MAP_XDG_DEFAULTS_WITH_MULTIPATH = {
+    "user_data_dir": XDGVariable("XDG_DATA_HOME", "~/.local/share"),
+    "site_data_dir": XDGVariable("XDG_DATA_DIRS", f"/usr/local/share{os.pathsep}/usr/share"),
+    "user_config_dir": XDGVariable("XDG_CONFIG_HOME", "~/.config"),
+    "site_config_dir": XDGVariable("XDG_CONFIG_DIRS", "/etc/xdg"),
+    "user_cache_dir": XDGVariable("XDG_CACHE_HOME", "~/.cache"),
+    "user_state_dir": XDGVariable("XDG_STATE_HOME", "~/.local/state"),
+    "user_log_dir": XDGVariable("XDG_LOG_HOME", "~/.cache"),
+}
+
+
+@pytest.mark.skipif(platform.system() != "Linux", reason="requires Linux")
+def test_xdg_variable_not_set(monkeypatch, func: str) -> None:
+    dirs = platformdirs.PlatformDirs("MyApp", "MyCompany", version="1.0", multipath=True, opinion=False)
+    xdg_variable = MAP_XDG_DEFAULTS_WITH_MULTIPATH[func]
+    monkeypatch.delenv(xdg_variable.name, raising=False)
+    result = getattr(dirs, func)
+    assert result == xdg_variable.default_value
+
+
+@pytest.mark.skipif(platform.system() != "Linux", reason="requires Linux")
+def test_xdg_variable_empty_value(monkeypatch, func: str) -> None:
+    dirs = platformdirs.PlatformDirs("MyApp", "MyCompany", version="1.0", multipath=True, opinion=False)
+    xdg_variable = MAP_XDG_DEFAULTS_WITH_MULTIPATH[func]
+    monkeypatch.setenv(xdg_variable.name, "")
+    result = getattr(dirs, func)
+    assert result == xdg_variable.default_value
+
+
+@pytest.mark.skipif(platform.system() != "Linux", reason="requires Linux")
+def test_xdg_variable_custom_value(monkeypatch, func: str) -> None:
+    dirs = platformdirs.PlatformDirs("MyApp", "MyCompany", version="1.0", multipath=True, opinion=False)
+    xdg_variable = MAP_XDG_DEFAULTS_WITH_MULTIPATH[func]
+    monkeypatch.setenv(xdg_variable.name, DIR_CUSTOM)
+    result = getattr(dirs, func)
+    assert result == DIR_CUSTOM
