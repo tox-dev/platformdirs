f91a841d808f76adee692352f82ee12a382f5a71
0az <0az@afzhou.com>
2021-08-03 00:22:40 -0500

    tests: Fix compat test regression

--- tests/test_comp_with_appdirs.py
+++ tests/test_comp_with_appdirs.py
@@ -3,8 +3,12 @@ from typing import Any, Dict
 
 import appdirs
 import pytest
+from pytest_mock import MockerFixture
 
 import platformdirs
+from platformdirs.unix import SUPPORTS_XDG
+
+from .common import OS
 
 
 def test_has_backward_compatible_class() -> None:
@@ -28,15 +32,37 @@ def test_has_backward_compatible_class() -> None:
         "app_name_author_version",
     ],
 )
-def test_compatibility(params: Dict[str, Any], func: str) -> None:
-    if sys.platform == "darwin":
+@pytest.mark.parametrize("system", ("darwin", "unix", "win32"))
+def test_compatibility(
+    mocker: MockerFixture,
+    params: Dict[str, Any],
+    system: str,
+    func: str,
+    mock_environ: Dict[str, str],
+) -> None:
+    if system == "darwin":
         msg = {  # pragma: no cover
             "user_log_dir": "without appname produces NoneType error",
             "site_config_dir": "ignores the version argument",
-            "user_config_dir": "uses Library/Preferences instead Application Support",
+            "user_config_dir": "uses Library/Preferences instead of Application Support",
         }
         if func in msg:  # pragma: no cover
-            pytest.skip(f"`appdirs.{func}` {msg[func]} on macOS")  # pragma: no cover
+            pytest.skip(
+                f"`appdirs.{func}` {msg[func]} on macOS"
+            )  # pragma: no cover
+        if SUPPORTS_XDG.get(func) in mock_environ:
+            pytest.skip(
+                f"`appdirs.{func}` ignores the XDG specification on macOS"
+            )
+    if system == "win32" and sys.platform != "win32":
+        pytest.skip("Windows tests only work on win32")  # pragma: no cover
+
+    mocker.patch("sys.platform", system)
+    mocker.patch("appdirs.system", system)
+    mocker.patch("platformdirs.PlatformDirs", OS[system])
+    # ASSUMPTION: For checking compat, we only care about the old location if
+    # it's on disk.
+    mocker.patch("os.path.exists", lambda _: True)
 
     new = getattr(platformdirs, func)(*params)
     old = getattr(appdirs, func)(*params)
