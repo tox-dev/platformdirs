20e6cea2eb925a808d1eae3e43e692d30976252a
Jeremy Stepp <git@jeremystepp.me>
2021-08-27 15:51:09 -0500

    Add user_documents_dir Unix tests

--- tests/test_unix.py
+++ tests/test_unix.py
@@ -4,8 +4,34 @@ import typing
 import pytest
 from _pytest.monkeypatch import MonkeyPatch
 
+import platformdirs.unix
 from platformdirs.unix import Unix
 
+
+def test_user_documents_dir(monkeypatch: MonkeyPatch) -> None:
+    example_path = "/home/example/ExampleDocumentsFolder"
+    monkeypatch.setattr(platformdirs.unix, "get_user_dirs_folder", lambda key: example_path)
+    assert Unix().user_documents_dir == example_path
+
+def test_user_documents_dir_env_var(monkeypatch: MonkeyPatch) -> None:
+    # Mock documents dir not being in user-dirs.dirs file
+    monkeypatch.setattr(platformdirs.unix, "get_user_dirs_folder", lambda key: None)
+
+    example_path = "/home/example/ExampleDocumentsFolder"
+    monkeypatch.setenv("XDG_DOCUMENTS_DIR", example_path)
+
+    assert Unix().user_documents_dir == example_path
+
+def test_user_documents_dir_default(monkeypatch: MonkeyPatch) -> None:
+    # Mock documents dir not being in user-dirs.dirs file
+    monkeypatch.setattr(platformdirs.unix, "get_user_dirs_folder", lambda key: None)
+    # Mock no XDG_DOCUMENTS_DIR env variable being set
+    monkeypatch.setenv("XDG_DOCUMENTS_DIR", "")
+
+    # Mock home directory
+    monkeypatch.setenv("HOME", "/home/example")
+
+    assert Unix().user_documents_dir == "/home/example/Documents"
 
 class XDGVariable(typing.NamedTuple):
     name: str
