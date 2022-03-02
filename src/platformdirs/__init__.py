1ca0d1c202dafd8195db7e47936cce790028d9f0
Blocked <4366503+Blocked@users.noreply.github.com>
2022-03-03 02:48:11 +0530

    refactor

--- src/platformdirs/__init__.py
+++ src/platformdirs/__init__.py
@@ -25,11 +25,11 @@ def _set_platform_dir_class() -> type[PlatformDirsABC]:
         from platformdirs.unix import Unix as Result
 
     if os.getenv("ANDROID_DATA") == "/data" and os.getenv("ANDROID_ROOT") == "/system":
-        from platformdirs.android import _android_folder
 
         if os.getenv("SHELL") is not None:
             return Result
 
+        from platformdirs.android import _android_folder
         if _android_folder() is not None:
             from platformdirs.android import Android
 
